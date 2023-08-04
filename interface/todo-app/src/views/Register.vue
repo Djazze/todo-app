6362cf42b3695252f2997fc12782e366a830f82c
Mateo Puđa <pudja.mateo@nsoft.com>
2023-08-04 14:12:06 +0200

    Registration route fix

--- interface/todo-app/src/views/Register.vue
+++ interface/todo-app/src/views/Register.vue
@@ -1,40 +1,45 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <!-- src/views/Register.vue -->
 <template>
-    <div>
-        <h1>Register</h1>
-        <form @submit.prevent="register">
-            <input type="text" v-model="username" placeholder="Username" required />
-            <input type="password" v-model="password" placeholder="Password" required />
-            <button type="submit">Register</button>
-        </form>
-    </div>
+  <div>
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+      <input type="text" v-model="username" placeholder="Username" required />
+      <input type="password" v-model="password" placeholder="Password" required />
+      <button type="submit">Register</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            username: '',
-            password: ''
-        };
-    },
-    methods: {
-        async register() {
-            // Send registration request to the backend
-            const response = await fetch('/api/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username: this.username, password: this.password })
-            });
 
-            // Handle response
-            if (response.ok) {
-                this.$router.push('/login');
-            } else {
-                // Handle error (e.g., show an error message)
-            }
+import axios from '@/axios-config';
+
+export default {
+  
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    async register() {
+      // Send registration request to the backend
+      try {
+        const response = await axios.post('/api/register', {
+          username: this.username,
+          password: this.password
+        });
+        if (response.status === 200) {
+          this.$router.push('/login');
+        } else {
+          // Handle error (e.g., show an error message)
         }
-    }
-};
+      } catch (error) {
+        // Handle error (e.g., show an error message)
+      }
+    },
+  },
+}
 </script>
