0ba80288ab86536b3248587e839948fda545b609
Mateo Puđa <pudja.mateo@nsoft.com>
2023-08-04 12:51:14 +0200

    VUP

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 3000;
 const app = express();
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: '',
+  password: '1234',
+  database: 'todo_app'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to the database!');
+});
+
+const users = {
+  create: (username, password, callback) => {
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    connection.query(query, [username, hashedPassword], callback);
+  },
+  findByUsername: (username, callback) => {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    connection.query(query, [username], callback);
+  }
+};
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -40,3 +67,18 @@ app.delete('/api/todo/:id', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    users.findByUsername(username, (err, results) => {
+      if (err) throw err;
+      const user = results[0];
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign({ username: user.username }, 'SECRET_KEY', { expiresIn: '1h' });
+        res.json({ token });
+      } else {
+        res.status(401).json({ error: 'Invalid username or password' });
+      }
+    });
+  });
+  
